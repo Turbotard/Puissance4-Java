bc92eb0d8960817d7a8e055fa04ecbb38d56fd34
Turbotard <turbotard9@gmail.com>
2023-01-24 10:34:22 +0100

    correction pour pas tout casser

--- src/IAniv1.java
+++ src/IAniv1.java
@@ -4,21 +4,23 @@ import java.util.Random;
 public class IAniv1 {
     public static Power4Grid grid;
     public void Pion(Power4Grid grid) {
-        this.grid = grid;
+        Pion.grid = grid;
     }
 
-    public static void RandomPion(int player) {
+    public static void RandomPion(String player) {
         Random rand = new Random();
-        int bot = rand.nextInt(6 - 0 + 1) + 0;
-        ArrayList<ArrayList<Integer>> currentGrid = grid.getGrid();
-        int randomIndex = (int) (Math.random() * Power4Grid.COLS);
-        int col = randomIndex;
+        int col = rand.nextInt(6 - 0) + 0;
+        ArrayList<ArrayList<String>> currentGrid = grid.getGrid();
         for (int row = Power4Grid.ROWS-1; row >= 0; row--) {
-            if (currentGrid.get(row).get(col) == 0) {
-                currentGrid.get(row).set(bot, player);
+            if (currentGrid.get(row).get(col) == "0") {
+                currentGrid.get(row).set(col, player);
                 grid.setGrid(currentGrid);
                 break;
             }
+            
+            
+
         }
-    }
+        col = rand.nextInt(6 - 0) + 0;
+    }     
 }
