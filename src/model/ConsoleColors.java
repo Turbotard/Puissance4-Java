579fe464c73a70a8035127c4aeca20826ff5b1d1
Turbotard <turbotard9@gmail.com>
2023-01-24 14:37:13 +0100

    blue

--- src/model/ConsoleColors.java
+++ src/model/ConsoleColors.java
@@ -7,7 +7,7 @@ public class ConsoleColors {
     public static final String GREEN = "\033[0;32m";
     public static final String DARK_GREEN = "\033[0;32;1m";
     public static final String YELLOW = "\033[0;33m";
-    public static final String BLUE = "\033[0;34m";
+    public static final String BLUE = "\u001B[34m";
     public static final String PURPLE = "\u001B[35m";
     public static final String CYAN = "\033[0;36m";
     
