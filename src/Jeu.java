1c9670b737b88d7d928a61fac836e7bdb6d0873e
Turbotard <turbotard9@gmail.com>
2023-01-25 12:08:49 +0100

    validations

--- src/Jeu.java
+++ src/Jeu.java
@@ -147,14 +147,14 @@ public class Jeu {
                         col = 1;
                         col--;
                         Pion.addPion(col, currentPlayer);
-                        player.changePlayer2();
+                        player.changePlayer();
                         mygrid.displayGrid();
                         break;
                     case "2":
                         col = 2;
                         col--;
                         Pion.addPion(col, currentPlayer);
-                        player.changePlayer2();
+                        player.changePlayer();
                         mygrid.displayGrid();
                         break;
                     case "3":
@@ -162,7 +162,7 @@ public class Jeu {
                         col--;
 
                         Pion.addPion(col, currentPlayer);
-                        player.changePlayer2();
+                        player.changePlayer();
                         mygrid.displayGrid();
                         break;
                     case "4":
@@ -170,7 +170,7 @@ public class Jeu {
                         col--;
 
                         Pion.addPion(col, currentPlayer);
-                        player.changePlayer2();
+                        player.changePlayer();
                         mygrid.displayGrid();
                         break;
                     case "5":
@@ -178,7 +178,7 @@ public class Jeu {
                         col--;
 
                         Pion.addPion(col, currentPlayer);
-                        player.changePlayer2();
+                        player.changePlayer();
                         mygrid.displayGrid();
                         break;
                     case "6":
@@ -186,7 +186,7 @@ public class Jeu {
                         col--;
 
                         Pion.addPion(col, currentPlayer);
-                        player.changePlayer2();
+                        player.changePlayer();
                         mygrid.displayGrid();
                         break;
                     case "7":
@@ -194,7 +194,7 @@ public class Jeu {
                         col--;
 
                         Pion.addPion(col, currentPlayer);
-                        player.changePlayer2();
+                        player.changePlayer();
                         mygrid.displayGrid();
                         break;
                     default:
