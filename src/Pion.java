d23ce6d2e8d7c9e5ea3c8483e2c2f5ad9df06e5a
Sambachpx <113524274+Sambachpx@users.noreply.github.com>
2023-01-25 15:31:19 +0100

    ça marche mieux manque diago

--- src/Pion.java
+++ src/Pion.java
@@ -5,11 +5,12 @@ public class Pion {
     public static Power4Grid grid;
     private static String pion;
 
-    public static String getPionIA(){
+    public static String getPionIA() {
         return pion;
     }
-    public static String setPionIA(){
-        pion = "x" ;
+
+    public static String setPionIA() {
+        pion = "x";
         return pion;
     }
 
@@ -19,16 +20,16 @@ public class Pion {
 
     public static boolean addPion(int col, String player) {
         ArrayList<ArrayList<String>> currentGrid = grid.getGrid();
-        if (Player.getCurrentPlayer() == "1"){
-            for (int row = Power4Grid.ROWS-1; row >= 0; row--) {
+        if (Player.getCurrentPlayer() == "1") {
+            for (int row = Power4Grid.ROWS - 1; row >= 0; row--) {
                 if (currentGrid.get(row).get(col) == "  ") {
                     currentGrid.get(row).set(col, Menu.couleur1);
                     grid.setGrid(currentGrid);
                     break;
                 }
             }
-        } else if (Player.getCurrentPlayer() == "2"){
-            for (int row = Power4Grid.ROWS-1; row >= 0; row--) {
+        } else if (Player.getCurrentPlayer() == "2") {
+            for (int row = Power4Grid.ROWS - 1; row >= 0; row--) {
                 if (currentGrid.get(row).get(col) == "  ") {
                     currentGrid.get(row).set(col, Menu.couleur2);
                     grid.setGrid(currentGrid);
@@ -44,16 +45,16 @@ public class Pion {
 
     public static boolean addPionBot(int col, String player) {
         ArrayList<ArrayList<String>> currentGrid = grid.getGrid();
-        if (Player.getCurrentPlayer() == "1"){
-            for (int row = Power4Grid.ROWS-1; row >= 0; row--) {
+        if (Player.getCurrentPlayer() == "1") {
+            for (int row = Power4Grid.ROWS - 1; row >= 0; row--) {
                 if (currentGrid.get(row).get(col) == "  ") {
                     currentGrid.get(row).set(col, Menu.couleur1);
                     grid.setGrid(currentGrid);
                     break;
                 }
             }
-        } else if (Player.getCurrentPlayer() == "AI"){
-            for (int row = Power4Grid.ROWS-1; row >= 0; row--) {
+        } else if (Player.getCurrentPlayer() == "AI") {
+            for (int row = Power4Grid.ROWS - 1; row >= 0; row--) {
                 if (currentGrid.get(row).get(col) == "  ") {
                     currentGrid.get(row).set(col, Menu.couleur2);
                     grid.setGrid(currentGrid);
@@ -74,7 +75,6 @@ public class Pion {
         bot = rand.nextInt(6 - 0) + 0;
     }
 
-    
     public void RandomPion1(String player) {
         Random rand = new Random();
         int random = rand.nextInt(6 - 0) + 0;
@@ -83,69 +83,70 @@ public class Pion {
         // Vérifie les lignes
         for (int row = 0; row < Power4Grid.ROWS; row++) {
             for (int col = 0; col < Power4Grid.COLS - 3; col++) {
-                if (currentGrid.get(row).get(col)==player
-                && currentGrid.get(row).get(col + 1)==player
-                && currentGrid.get(row).get(col + 2)==player
-                && currentGrid.get(row).get(col + 3)=="  ") {
-                        System.out.println("ligne2");
-                        addPion(col+3, Menu.couleur2);
-                        jouer = true;
-                        break;
-                    
+                if (currentGrid.get(row).get(col).equals(player)
+                        && currentGrid.get(row).get(col + 1).equals(player)
+                        && currentGrid.get(row).get(col + 2).equals(player)
+                        && currentGrid.get(row).get(col + 3).equals("  ")) {
+                    System.out.println("ligne");
+                    addPion(col + 3, Menu.couleur2);
+                    jouer = true;
+                    break;
+
                 }
             }
         }
 
-          // Vérifie les colonnes
-        for (int col = 0; col < Power4Grid.COLS; col++) {
-            for (int row = 0; row < Power4Grid.ROWS-3; row++) {
-                if (currentGrid.get(row).get(col)==player 
-                    && currentGrid.get(row + 1).get(col)==player 
-                    && currentGrid.get(row + 2).get(col)==player
-                    && currentGrid.get(row + 3 ).get(col)=="  "){
-                        System.out.println("colonne");
+       // Verifie les colonnes
+    for (int col = 0; col < Power4Grid.COLS; col++) {
+        for (int row = 5; row >= 0 ; row--) {
+            if (currentGrid.get(row).get(col).equals(player)
+                    && currentGrid.get(row - 1).get(col).equals(player)
+                    && currentGrid.get(row - 2).get(col).equals(player)
+                    && currentGrid.get(row - 3).get(col).equals("  ")) {
+                System.out.println("colonne");
 
-                        addPion(row+3, Menu.couleur2);
-                        jouer = true;
+                addPion(col, Menu.couleur2);
+                jouer = true;
 
-                        break;
-                    } 
+                break;
+            }
 
-                }
         }
+    }
+
 
         // Vérifie les diagonales
 
-        for (int row = 0; row < Power4Grid.ROWS-3; row++) {
+        for (int row = 0; row < Power4Grid.ROWS - 3; row++) {
             for (int col = 0; col < Power4Grid.COLS - 3; col++) {
-                if (currentGrid.get(row).get(col) ==player
-                        && currentGrid.get(row + 1).get(col + 1)==player
-                        && currentGrid.get(row + 2).get(col + 2)==player
+                if (currentGrid.get(row).get(col).equals(player)
+                        && currentGrid.get(row + 1).get(col + 1).equals(player)
+                        && currentGrid.get(row + 2).get(col + 2).equals(player)
                         && currentGrid.get(row + 2).get(col + 1).equals("  ")) {
-                            System.out.println("diago");
-                            addPion(col+3, Menu.couleur2);
-                            jouer = true;
+                    System.out.println("diago");
+                    addPion(col + 3, Menu.couleur2);
+                    jouer = true;
 
                     break;
                 }
 
-                if (currentGrid.get(row).get(col + 3)==player
-                        && currentGrid.get(row + 1).get(col + 2)==player
-                        && currentGrid.get(row + 2).get(col + 1)==player) {
-                            System.out.println("diago2");
-                            addPion(col+3, Menu.couleur2);
-                            jouer = true;
+                if (currentGrid.get(row).get(col + 3).equals(player)
+                        && currentGrid.get(row + 1).get(col + 2).equals(player)
+                        && currentGrid.get(row + 2).get(col + 1).equals(player)) {
+                    System.out.println("diago2");
+                    addPion(col + 3, Menu.couleur2);
+                    jouer = true;
 
                     break;
                 }
             }
         }
 
-        if (!jouer){
-            random = rand.nextInt(6 - 0) + 0;;
+        if (!jouer) {
+            random = rand.nextInt(6 - 0) + 0;
+            System.out.println("pif");
             addPion(random, Menu.couleur2);
         }
-        
 
     }
 }
