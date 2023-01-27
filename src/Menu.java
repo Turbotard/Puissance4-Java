de952551dee598b0f48d7bcd284a3b98a10b3576
EstebanDardillac <esteban.dardillac@gmail.com>
2023-01-27 15:21:02 +0100

    documentation

--- src/Menu.java
+++ src/Menu.java
@@ -1,116 +1,26 @@
 
 import java.io.IOException;
 import java.text.ParseException;
-//import java.io.FileNotFoundException;
-//import java.io.FilePermission;
 import java.io.BufferedReader;
-// import java.io.BufferedWriter;
-// import java.io.File;
-// import java.io.BufferedWriter;
 import java.io.FileReader;
-//import java.io.FileWriter;
-// import java.io.FileWriter;
-// import java.io.IOException;
-// import java.io.PrintWriter;
-// import java.util.ArrayList;
-// import java.util.stream.Collectors;
 import java.util.ArrayList;
 import model.ConsoleColors;
 import model.CustomeUtils;
-//import model.Classement;
-
 import java.util.*;
 /**
  * Ensemble des méthodes qui permettent d'afficher différents menus et gérer les
  * choix de l'utilisateur
  */
-public class Menu {
+public class Menu { // classe mneu
+    
+    // intialisation de variables
     public static String couleur1 = "💙";
     public static String couleur2 = "💔";
-    
-
-    // public static void lister() throws IOException{
-    //     String pathFile = "src/Top10.csv";
-    //     String line = " ";
-
-    //     try{
-    //         BufferedReader br = new BufferedReader(new FileReader(pathFile));
-    //         while((line =br.readLine())!=null){
-    //             System.out.println(line);
-    //         }
-    //         System.out.println("File found");
-    //     }
-
-    //     catch(FileNotFoundException e){
-    //         e.printStackTrace();
-    //         System.out.println("File not found");
-    //     }
-    // }
     static int bestScore = 0;
     static String bestPlayer = "";
     public static Object main;
 
-
-
-
-    // static void top10ToCsv() {
-    //     try (BufferedWriter bw = new BufferedWriter(new FileWriter("Top10.csv"))) {
-    //         for (Classement score : topTen) {
-    //             String contactString = contact.toString();
-    //             bw.write(contactString);
-    //             bw.newLine();
-    //         }
-    //     } catch (IOException e) {
-    //         e.printStackTrace();
-    //     }
-    // }
-
-
-
-
-    // public static void lister(String[] args) {
-    //     String csvFile = "Top10.csv";
-    //     String line = "";
-    //     String cvsSplitBy = ";";
-    //     try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
-    //         while ((line = br.readLine()) != null) {
-    //             String[] player = line.split(cvsSplitBy);
-    //             String name = player[0];
-    //             int score = Integer.parseInt(player[1]); 
-    //             if (score > bestScore) {
-    //                 bestScore = score;
-    //                 bestPlayer = name;
-    //             }
-    //         }
-    //     } catch (IOException e) {
-    //         e.printStackTrace();
-    //     }
-    //     System.out.println("Le joueur avec le meilleur score est " + bestPlayer + " avec " + bestScore + " points.");
-    // }
-
-
-
-    
-    // public static void lister(String filePath) {
-    //     List<String> scores = new ArrayList<>();
-    //     String line;
-    //     String cvsSplitBy = ";";
-    //     try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
-    //         while ((line = br.readLine()) != null) {
-    //             String[] score = line.split(cvsSplitBy);
-    //             scores.add(score[0].toString());
-    //         }
-    //     } catch (IOException e) {
-    //         e.printStackTrace();
-    //     }
-    //     Collections.sort(scores, Collections.reverseOrder());
-    //     for (String score : scores) {
-    //         System.out.println(score);
-    //     }
-    // }
-    
-
-    public static void lister(String filePath) {
+    public static void lister(String filePath) { // fonction pour afficher le top 10 des meilleurs joueurs / autheur : Lucas
         List<String[]> scores = new ArrayList<>();
         String line;
         String cvsSplitBy = ";";
@@ -128,28 +38,11 @@ public class Menu {
         }
     }
 
-
-
-
-
-    // public void topTenToCsv() {
-
-    //     try (BufferedWriter pw = new BufferedWriter(new FileWriter(this.filename))) {
-    //         for (Classement score : topTen) {
-    //             String stringScore = score.toString();
-    //             pw.write(stringScore);
-    //             pw.newLine();
-    //         }
-    //     } catch (IOException e) {
-    //         System.out.println("Erreur lors de l'écriture du fichier");
-    //     }
-    // }
-
     /**
      * Affiche le menu principal et gère les choix de l'utilisateur
      * @throws IOException
      */
-    public static void displayMenu() throws ParseException, IOException {
+    public static void displayMenu() throws ParseException, IOException {  // fontion permettant d'afficher le menu principal / autheur : Esteban
         do {
             System.out.println(ConsoleColors.GREEN + "  -- Menu --");
             System.out.println("1. Jouer en solo");
@@ -171,9 +64,6 @@ public class Menu {
                     break;
                 case "3":
                     lister("Top10.csv");
-                    //Classement.afficherClassement();
-                    //rator.compare();
-                    //ScoreComparator.main(null);
                     break;
                 case "q":
                     quit();
@@ -185,9 +75,10 @@ public class Menu {
         } while (true);
     }
 
-public static int lvl;
-public static String couleurIA;
-    public static void DiffIA() throws ParseException {
+    public static int lvl;
+    public static String couleurIA;
+
+    public static void DiffIA() throws ParseException {  // fonction pour afficher le menu de difficultées de IA / autheurs : Esteban et Samba 
 
         System.out.println(ConsoleColors.GREEN + "1. difficulté 1" + ConsoleColors.DEFAULT);
         System.out.println(ConsoleColors.YELLOW + "2. difficulté 2" + ConsoleColors.DEFAULT);
@@ -230,11 +121,11 @@ public static String couleurIA;
     }
     
 
-    public static void quit() {
+    public static void quit() { // fonction du message de sortie de menu / autheur : Esteban
         System.out.println(ConsoleColors.RED + "Fermeture du menu..." + ConsoleColors.DEFAULT);
     }
 
-    public static void settingP1(){
+    public static void settingP1(){  // fonction pour paramétrer le joueur 1 / autheur : Esteban
         Player.currentPlayer ="1";
         System.out.println("Joueur1");
         System.out.println("Veuillez choisir le nom de votre choix");
@@ -243,14 +134,11 @@ public static String couleurIA;
         try {
             displayMenuCouleurP1();
         } catch (ParseException e) {
-            // TODO Auto-generated catch block
             e.printStackTrace();
         }
-        /*System.out.println("Veuillez choisir la couleur de votre choix");
-        Player.getCouleur();*/
     }
 
-    public static void settingP2(){
+    public static void settingP2(){  // fonction pour paramétrer le joueur 2 / autheur : Esteban
         Player.currentPlayer ="2";
         System.out.println("Joueur 2");
         System.out.println("Veuillez choisir le nom de votre choix");
@@ -259,21 +147,18 @@ public static String couleurIA;
         try {
             displayMenuCouleurP2();
         } catch (ParseException e) {
-            // TODO Auto-generated catch block
             e.printStackTrace();
         }
 
     }
-    public static void settingIA(){
+    public static void settingIA(){  // fonction de paramètre de l'IA / autheur : Esteban
         Player.currentPlayer ="2";
         Player.pseudo2 ="IA";
-        Player.pion2 = "X";
-
     }
 
 
-    public static void displayMenuCouleurP1() throws ParseException {
-        do {
+    public static void displayMenuCouleurP1() throws ParseException {  // fonction pour paramétrer le pion du joueur 1 avec un menu
+        do {                                                           // autheurs : Esteban et Benjamin
             System.out.println(ConsoleColors.GREEN + "  -- Menu Couleur--");
             System.out.println("1. 💖");
             System.out.println("2. 💚");
@@ -315,8 +200,8 @@ public static String couleurIA;
             }
         } while (true);
     }
-    public static void displayMenuCouleurP2() throws ParseException {
-        do {
+    public static void displayMenuCouleurP2() throws ParseException {  // fonction pour paramétrer le pion du joueur 2 avec un menu                                                                  
+        do {                                                           // autheurs : Esteban et Benjamin
             System.out.println(ConsoleColors.GREEN + "  -- Menu Couleur--");
             System.out.println("1. 💖");
             System.out.println("2. 💚");
@@ -359,7 +244,4 @@ public static String couleurIA;
             }
         } while (true);
     }
-
-
-   
 }
